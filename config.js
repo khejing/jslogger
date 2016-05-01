ff55ceaf88ec7b2cfa30519c234db97f84e84c1c
khejing <khejing@hotmail.com>
2016-05-01 23:21:08 +0800

    initial commit

---
+++ config.js
@@ -0,0 +1,4 @@
+let ServerDomain = "42.62.93.120";
+let LogPort = 8081;
+let LOGURL = "http://"+ServerDomain+":"+LogPort;
+export {LOGURL};
---
+++ index.js
@@ -0,0 +1,19 @@
+import {LOGURL} from './config.js';
+import Log4js from 'log4js-dist';
+
+let logger;
+if(DEVICE_TYPE === 'mobile' && PLATFORM === 'android'){
+    logger = new Log4js.getLogger("android");
+}else{
+    logger = new Log4js.getLogger("desktop")
+}
+let ajaxAppender = new Log4js.AjaxAppender(LOGURL+"/inputs/d00dadc0ffee");
+let jsonLayout = new Log4js.JSONLayout();
+ajaxAppender.setLayout(jsonLayout);
+
+logger.setDateFormat("yyyy-MM-ddthh:mm:ss.SSSO");
+
+logger.setLevel(Log4js.Level.DEBUG);
+logger.addAppender(ajaxAppender);
+
+export default logger;
