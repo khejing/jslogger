f954aaa3a50cf533fea7589bc0931cac125c28b0
lizhi <zhi.li@yunshipei.com>
2016-08-30 17:55:31 +0800

    配置要传进来，库项目不应包含配置

--- config.js
+++
@@ -1,4 +0,0 @@
-let ServerDomain = "42.62.93.120";
-let LogPort = 8081;
-let LOGURL = "http://"+ServerDomain+":"+LogPort;
-export {LOGURL};
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import {LOGURL} from './config.js';
 import Log4js from 'log4js-dist';
 import {isWeChat, isAndroid, isiPhone} from 'ua.js';
 
@@ -16,19 +15,25 @@ function getLogger(){
   return new Log4js.getLogger("Desktop")
 }
 
-let logger = getLogger();
+function Logger({serverDomain, port, isAppendingConsole}){
+  let logger = getLogger();
 
-let jsonLayout = new Log4js.JSONLayout();
+  let jsonLayout = new Log4js.JSONLayout();
 
-let ajaxAppender = new Log4js.AjaxAppender(LOGURL+"/inputs/d00dadc0ffee");
-ajaxAppender.setLayout(jsonLayout);
-logger.addAppender(ajaxAppender);
+  let ajaxAppender = new Log4js.AjaxAppender("http://"+serverDomain+":"+port+"/inputs/d00dadc0ffee");
+  ajaxAppender.setLayout(jsonLayout);
+  logger.addAppender(ajaxAppender);
 
-let consoleAppender = new Log4js.BrowserConsoleAppender();
-consoleAppender.setLayout(jsonLayout);
-logger.addAppender(consoleAppender);
+  if(isAppendingConsole){
+    let consoleAppender = new Log4js.BrowserConsoleAppender();
+    consoleAppender.setLayout(jsonLayout);
+    logger.addAppender(consoleAppender);
+  }
 
-logger.setDateFormat("yyyy-MM-ddthh:mm:ss.SSSO");
-logger.setLevel(Log4js.Level.DEBUG);
+  logger.setDateFormat("yyyy-MM-ddthh:mm:ss.SSSO");
+  logger.setLevel(Log4js.Level.DEBUG);
 
-export default logger;
+  return logger;
+}
+
+export default Logger;
